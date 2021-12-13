a734f4ed76bdab228c8456df627c944bf703a65a
JJukyungs <wnrud675@naver.com>
2021-12-14 01:26:50 +0900

    firebase 연결은 했는 데 .... 테스트 예제가 제대로 실행이 안된다 왜 안될까?

--- SeSac_MyMealLog/Controller/ListViewController.swift
+++ SeSac_MyMealLog/Controller/ListViewController.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAnalytics
+import FirebaseCrashlytics
+
+
 
 class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 오류 구현 코드
+        let button = UIButton(type: .roundedRect)
+           button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+           button.setTitle("Test Crash", for: [])
+           button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+           view.addSubview(button)
+        
     }
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+         let numbers = [0]
+         let _ = numbers[1]
+     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+      
+        // 회원가입 : 아이디 > 닉네임 > 연락처 > 성별 > 가입 완료
+        
+//        Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
+//          AnalyticsParameterItemID: "id-\(title!)",
+//          AnalyticsParameterItemName: title!,
+//          AnalyticsParameterContentType: "cont",
+//        ])
+//
+        
+        Analytics.logEvent("share_image", parameters: [
+          "name": "Jack" as NSObject,
+          "full_text": "TEST" as NSObject,
+        ])
     }
-    */
 
 }
--- SeSac_MyMealLog/View/AppDelegate.swift
+++ SeSac_MyMealLog/View/AppDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 import IQKeyboardManagerSwift
 import Firebase
+import FirebaseCore
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
